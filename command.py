903303a2c15fbcd881d215338d571046ef4a92ec
Joao-Terencio <jh.terencio@gmail.com>
2024-03-07 15:49:40 -0300

    Making the remote control and implement the undo method

--- command.py
+++ command.py
@@ -1,55 +1,90 @@
 from abc import ABCMeta, abstractstaticmethod
 
-#Icommand Interface
+# * Icommand Interface
 class ICommand(metaclass=ABCMeta):
   
   @abstractstaticmethod
   def execute():
     """Método de uma interface estática"""
     
-# Lampada
+  def undo():
+    """Método de uma interface estática"""
+
+# * Comandos
+class Ligar(ICommand):
+  def __init__(self, dispositivo):
+        self.dispositivo = dispositivo
+
+  def execute(self):
+      self.dispositivo.ligar()
+
+  def undo(self):
+      self.dispositivo.desligar()
+
+class Desligar(ICommand):
+  def __init__(self, dispositivo):
+        self.dispositivo = dispositivo
+
+  def execute(self):
+      self.dispositivo.desligar()
+
+  def undo(self):
+      self.dispositivo.ligar()
+
+class Aumentar(ICommand):
+  def __init__(self, dispositivo):
+        self.dispositivo = dispositivo
+
+  def execute(self):
+      self.dispositivo.aumentar()
+
+  def undo(self):
+      self.dispositivo.diminuir()
+      
+class Diminuir(ICommand):
+  def __init__(self, dispositivo):
+        self.dispositivo = dispositivo
+
+  def execute(self):
+      self.dispositivo.diminuir()
+
+  def undo(self):
+      self.dispositivo.aumentar()
+class TrocarCanal(ICommand):
+  def __init__(self, televisao):
+    self._televisao = televisao
+    
+  def execute(self):
+    self._televisao.trocar_canal()      
+    
+# * Lampada
 class Lampada:
   
-  def ligar_lampada(self):
+  def ligar(self):
     print("Lampada foi ligada")
   
-  def desligar_lampada(self):
+  def desligar(self):
     print("Lampada foi desligada")
     
-# Comandos de lampada
-class LigarLampada(ICommand):
-  def __init__(self, lampada):
-    self._lampada = lampada
-    
-  def execute(self):
-    self._lampada.ligar_lampada()
-
-class DesligarLampada(ICommand):
-  def __init__(self, lampada):
-    self._lampada = lampada
-    
-  def execute(self):
-    self._lampada.desligar_lampada()
-
-# Televisão
+# * Televisão
 class Televisao:
     def __init__(self):
         self.volume = 0
         
-    def ligar_televisao(self):
+    def ligar(self):
       print("Televisão foi ligada")
   
-    def desligar_televisao(self):
+    def desligar(self):
       print("Televisão foi desligada")
 
-    def aumentar_volume_televisao(self):
+    def aumentar(self):
       if self.volume < 100:
         self.volume += 10
         print(f"O volume da TV foi aumentado para {self.volume}")
       else: 
         print("Volume da TV está no máximo")
 
-    def diminuir_volume_televisao(self):
+    def diminuir(self):
       if self.volume > 0:
         self.volume -= 10
         print(f"O volume da TV foi diminuido para {self.volume}")
@@ -58,156 +93,97 @@ class Televisao:
 
     def trocar_canal(self):
         print(f"O canal da TV foi trocado")
-
-# Comandos de Televisão
-class LigarTelevisao(ICommand):
-  def __init__(self, televisao):
-    self._televisao = televisao
     
-  def execute(self):
-    self._televisao.ligar_televisao()
-
-class DesligarTelevisao(ICommand):
-  def __init__(self, televisao):
-    self._televisao = televisao
-    
-  def execute(self):
-    self._televisao.desligar_televisao()
-
-class AumentarVolumeTelevisao(ICommand):
-  def __init__(self, televisao):
-    self._televisao = televisao
-    
-  def execute(self):
-    self._televisao.aumentar_volume_televisao()
-
-class DiminuirVolumeTelevisao(ICommand):
-  def __init__(self, televisao):
-    self._televisao = televisao
-    
-  def execute(self):
-    self._televisao.diminuir_volume_televisao()
-
-class TrocarCanal(ICommand):
-  def __init__(self, televisao):
-    self._televisao = televisao
-    
-  def execute(self):
-    self._televisao.trocar_canal()
-    
-# Sistema de som
+# * Sistema de som
 class SistemaDeSom:
     def __init__(self):
         self.volume = 0
 
-    def ligar_som(self):
+    def ligar(self):
       print("Som foi ligado")
   
-    def desligar_som(self):
+    def desligar(self):
       print("Som foi desligado")
     
-    def aumentar_volume_som(self):
+    def aumentar(self):
       if self.volume < 100:
         self.volume += 10
         print(f"O volume do sistema de som foi aumentado para {self.volume}")
       else: 
         print("Volume do sistema de som está no máximo")
 
-    def diminuir_volume_som(self):
+    def diminuir(self):
       if self.volume > 0:
         self.volume -= 10
         print(f"O volume do sistema de som foi diminuido para {self.volume}")
       else:
         print("Volume do sistema de som está no mínimo")
 
-# Comandos de Som
-class LigarSom(ICommand):
-  def __init__(self, som):
-    self._som = som
-    
-  def execute(self):
-    self._som.ligar_som()
-
-class DesligarSom(ICommand):
-  def __init__(self, som):
-    self._som = som
-    
-  def execute(self):
-    self._som.desligar_som()
-
-class AumentarVolumeSom(ICommand):
-  def __init__(self, som):
-    self._som = som
-    
-  def execute(self):
-    self._som.aumentar_volume_som()
-
-class DiminuirVolumeSom(ICommand):
-  def __init__(self, som):
-    self._som= som
-    
-  def execute(self):
-    self._som.diminuir_volume_som()
-
-# Invocador
+# * Controle remoto
 class ControleRemoto:
   def __init__(self):
-    self._comandos = {}
-    
-  def register(self, nome_comando, comando):
-    self._comandos[nome_comando] = comando
-  
-  def execute(self, nome_comando):
-    if nome_comando in self._comandos:
-      self._comandos[nome_comando].execute()
-    else:
-      print("Comando não encontrado")
+        self.comandos = []
 
-#Cliente
+  def registro(self, comando):
+      self.comandos.append(comando)
+
+  def execute(self):
+      for comando in self.comandos:
+          comando.execute()
+
+  def undo(self):
+      if self.comandos:
+          self.comandos.pop().undo()
+      else:
+          print("Nada para desfazer")
+
+# * Cliente
 if __name__ == "__main__":
-  # Dispositivos
+  # * Dispositivos
   LAMPADA = Lampada()
   TELEVISAO = Televisao()
   SOM = SistemaDeSom()
   
-  # Comandos
-  LIGAR_LAMPADA = LigarLampada(LAMPADA)
-  DESLIGAR_LAMPADA = DesligarLampada(LAMPADA)
+  # * Comandos
+  LIGAR_LAMPADA = Ligar(LAMPADA)
+  DESLIGAR_LAMPADA = Desligar(LAMPADA)
   
-  LIGAR_TELEVISAO = LigarTelevisao(TELEVISAO)
-  DESLIGAR_TELEVISAO = DesligarTelevisao(TELEVISAO)
-  AUMENTAR_VOLUME_TELEVISAO = AumentarVolumeTelevisao(TELEVISAO)
-  DIMINUIR_VOLUME_TELEVISAO = DiminuirVolumeTelevisao(TELEVISAO)
+  LIGAR_TELEVISAO = Ligar(TELEVISAO)
+  DESLIGAR_TELEVISAO = Desligar(TELEVISAO)
+  AUMENTAR_VOLUME_TELEVISAO = Aumentar(TELEVISAO)
+  DIMINUIR_VOLUME_TELEVISAO = Diminuir(TELEVISAO)
   TROCAR_CANAL = TrocarCanal(TELEVISAO)
   
-  LIGAR_SOM = LigarSom(SOM)
-  DESLIGAR_SOM = DesligarSom(SOM)
-  AUMENTAR_VOLUME_SOM = AumentarVolumeSom(SOM)
-  DIMINUIR_VOLUME_SOM = DiminuirVolumeSom(SOM)
+  LIGAR_SOM = Ligar(SOM)
+  DESLIGAR_SOM = Desligar(SOM)
+  AUMENTAR_VOLUME_SOM = Aumentar(SOM)
+  DIMINUIR_VOLUME_SOM = Diminuir(SOM)
   
-  # Controle remoto
+  # * Controle remoto
   CONTROLE_REMOTO = ControleRemoto()
   
-  CONTROLE_REMOTO.register("Ligar lampada", LIGAR_LAMPADA)
-  CONTROLE_REMOTO.register("Desligar lampada", DESLIGAR_LAMPADA)
-  CONTROLE_REMOTO.register("Ligar televisão", LIGAR_TELEVISAO)
-  CONTROLE_REMOTO.register("Desligar televisão", DESLIGAR_TELEVISAO)
-  CONTROLE_REMOTO.register("Aumentar volume televisão", AUMENTAR_VOLUME_TELEVISAO)
-  CONTROLE_REMOTO.register("Diminuir volume televisão", DIMINUIR_VOLUME_TELEVISAO)
-  CONTROLE_REMOTO.register("Trocar de canal", TROCAR_CANAL)
-  CONTROLE_REMOTO.register("Ligar sistema de som", LIGAR_SOM)
-  CONTROLE_REMOTO.register("Desligar sistema de som", DESLIGAR_SOM)
-  CONTROLE_REMOTO.register("Aumentar volume sistema de som", AUMENTAR_VOLUME_SOM)
-  CONTROLE_REMOTO.register("Diminuir volume sistema de som", DIMINUIR_VOLUME_SOM)
+  # * Ações a serem feitas
+  CONTROLE_REMOTO.registro(LIGAR_LAMPADA)
+  CONTROLE_REMOTO.registro(DESLIGAR_LAMPADA)
+  CONTROLE_REMOTO.registro(LIGAR_TELEVISAO)
+  CONTROLE_REMOTO.registro(DESLIGAR_TELEVISAO)
+  CONTROLE_REMOTO.registro(AUMENTAR_VOLUME_TELEVISAO)
+  CONTROLE_REMOTO.registro(DIMINUIR_VOLUME_TELEVISAO)
+  CONTROLE_REMOTO.registro(TROCAR_CANAL)
+  CONTROLE_REMOTO.registro(LIGAR_SOM)
+  CONTROLE_REMOTO.registro(DESLIGAR_SOM)
+  CONTROLE_REMOTO.registro(AUMENTAR_VOLUME_SOM)
+  CONTROLE_REMOTO.registro(DIMINUIR_VOLUME_SOM)
   
-  CONTROLE_REMOTO.execute("Ligar lampada")
-  CONTROLE_REMOTO.execute("Desligar lampada")
-  CONTROLE_REMOTO.execute("Ligar televisão")
-  CONTROLE_REMOTO.execute("Desligar televisão")
-  CONTROLE_REMOTO.execute("Aumentar volume televisão")
-  CONTROLE_REMOTO.execute("Diminuir volume televisão")
-  CONTROLE_REMOTO.execute("Trocar de canal")
-  CONTROLE_REMOTO.execute("Ligar sistema de som")
-  CONTROLE_REMOTO.execute("Desligar sistema de som")
-  CONTROLE_REMOTO.execute("Aumentar volume sistema de som")
-  CONTROLE_REMOTO.execute("Diminuir volume sistema de som")
+  # * Método que irá executar todas as ações
+  CONTROLE_REMOTO.execute()
+  
+  # * Desfaz a ultima ação executada
+  CONTROLE_REMOTO.undo()
+  
+# * EXPLICAÇÂO:
+  # * O padrão Command é um padrão de design comportamental que permite encapsular solicitações como objetos, permitindo a parametrização de operações e fornecendo suporte para histórico e reversão de ações.
+  
+  # * Parametrização de operações: Refere-se à capacidade de passar diferentes parâmetros para um comando, permitindo que ele realize diferentes ações em diferentes objetos sem modificar o próprio comando. Nesse meu código cada comando, como ligar, desligar, aumentar, diminuir e trocar de canal é responsável por conhecer o objeto que irá realizar a operação. Isso permite que você crie um novo comando apenas fornecendo o dispositivo relevante.
+  
+  # * Histórico e reversão de ações: O controle remoto mantém uma lista de comandos executados. Quando você desfaz uma ação, ele simplesmente chama o método undo do último comando na lista. Isso permite um histórico reversível de ações, útil para desfazer e refazer operações conforme necessário.
